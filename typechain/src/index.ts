7a589836ac29572e35326760a36c4cc864951b9f
hwahyeon <hwahyeon@knou.ac.kr>
2022-07-18 17:48:48 +0900

    Create the class of blockchain

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -20,5 +20,37 @@ class Block implements BlockShape{
     }
     static calculateHash(prevHash:string, height:number, data:string){
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[]
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data:string){
+        const newBlock = new Block(
+            this.getPrevHash(),
+            this.blocks.length + 1,
+            data);
+        this.blocks.push(newBlock);
+    }
+    public getBlocks(){
+        return [...this.blocks];
+    }
+}
+
+
+const blockchain = new Blockchain();
+blockchain.addBlock("Frist one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+blockchain.getBlocks().push(new Block("xxxxx", 11111, "HACKED"));
+
+console.log(blockchain.getBlocks());
